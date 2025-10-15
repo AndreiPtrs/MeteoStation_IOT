8762499b1c194663bc32bfdf4afae6cbaf91f8d6
GabiPtrs <andreigabi.petrisor@gmail.com>
2025-10-15 21:16:20 +0300

    Update WiFi credentials in Private.h to restore original settings

--- include/Private.h
+++ include/Private.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// #define ssid "Vodafone_WiFi_Students"
-// #define password "Future8T@len!s"
-#define ssid "525"
-#define password "NuSeSpune"
+#define ssid "Vodafone_WiFi_Students"
+#define password "Future8T@len!s"
+// #define ssid "525"
+// #define password "NuSeSpune"
